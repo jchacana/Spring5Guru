ebc991ab247250dff977ce018f2529b5c2a2b87f
Javier Chacana <jchacana@nisum.com>
2017-12-28 18:20:58 -0300

    Spring Data Repository

--- di-demo/src/main/java/guru/springframework/DiDemoApplication.java
+++ di-demo/src/main/java/guru/springframework/DiDemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {"package1", "package2"})
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
