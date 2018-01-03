4902bdee3f5855289b048d62b9d9ba08cbed64ac
Javier Chacana <jchacana@nisum.com>
2018-01-03 16:40:30 -0300

    Unit testing

--- spring5-recipe-app-many-to-many-jpa-example/src/main/java/guru/springframework/controllers/IndexController.java
+++ spring5-recipe-app-many-to-many-jpa-example/src/main/java/guru/springframework/controllers/IndexController.java
@@ -10,8 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
-
 /**
  * Created by jt on 6/1/17.
  */
